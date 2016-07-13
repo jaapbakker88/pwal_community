2d1b61eadd7f1fe65faae432ca6e66ccb8100b59
Jaap Bakker <hello@bakkerlabs.nl>
2016-07-13 10:25:02 +0200

    added reviews for adam sidney hazelina

--- javascripts/data.js
+++ javascripts/data.js
@@ -16,32 +16,18 @@ var users = [
     story: 'http://blog.partywithalocal.com/2016/03/10/story-amsterdam-party-night/'
   },
   {
+    name: 'Sidñey O.',
+    location: 'New York',
+    review: 'Sidñey is always up to the most fun and totally random things in NYC - dressing up as bacon, a lamp, a horse, a submarine... bodypainting parties, trampolining on skyscrapers… amazingly well connected and always looking to shake things up and have fun. Never a dull moment with Sidñey!',
+    image: 'sidney.jpg',
+    bio: 'Lived in and around New York since \'91. Have found a couple of semi-underground things that interest me, and might interest an intrepid traveler or stifled local. Burning Man parties, urban spelunking (aka urban exploration), society ball, guerrilla balls, skating (roller and ice), dancing (Latin, Swing, and freeform), concept and performance Art!'
+  },
+  {
     name: 'Baiba V.',
     location: 'Amsterdam',
     review: ' Always up for a good time and lots of laughs. Whether it\'s a chilled brunch, some outdoor activities or a full on night out, Baiba is always so much fun. She has a great sense of direction too and knows the city. Good to have on your team.',
     image: 'baiba.jpg',
     bio: 'Hey Pwalsters, Originally from Riga, Latvia but currently living in Amsterdam! Live traveling, have lived in Berlin, Cambridge and Edinburgh before moving here. I like meeting new people, so message me if you\'re in town. Toodles'
-  },
-  {
-    name: 'Hazelina O.',
-    location: 'Singapore',
-    review: '',
-    image: 'hazel.jpg',
-    bio: 'Deep house and Techno all night long, but also good old wine and banter. London, Amsterdam, Barcelona & Berlin in August for work and some partying. Let\'s prance?'
-  },
-  {
-    name: 'Anna C.',
-    location: 'New York',
-    review: 'Anna is super open and very nice. We went to a quiet party where she was the DJ. She got me in for free and introduced me to her friends. 100% recommendable!',
-    image: 'anna.jpg',
-    bio: 'Annakickarse, the outgoing rebel, is an artistic phenomenon from New York City who has worked her way from nothing to something.  I am skin, bones, blood, flesh & energy. I am and I believe that is all I could truly know about myself for certain. DJ from Webster Hall NYC & resident DJ at QuietEvents.'
-  },
-  {
-    name: 'Sidñey O.',
-    location: 'New York',
-    review: '',
-    image: 'sidney.jpg',
-    bio: 'Lived in and around New York since \'91. Have found a couple of semi-underground things that interest me, and might interest an intrepid traveler or stifled local. Burning Man parties, urban spelunking (aka urban exploration), society ball, guerrilla balls, skating (roller and ice), dancing (Latin, Swing, and freeform), concept and performance Art!'
   },
   {
     name: 'Brent C.',
@@ -51,6 +37,27 @@ var users = [
     bio: 'I love to show new people some of Melbourne\'s best rooftop drinking spots, hidden bars and nightclubs...Just ask!'
   },
   {
+    name: 'Hazelina O.',
+    location: 'Singapore',
+    review: 'Met Hazel in a barcelona techno clubbing night in the summer, and we met up again in singapore for lots and lots of gin & tonics. Really warm and fun girl, hope to see you again somewhere in the world!',
+    image: 'hazel.jpg',
+    bio: 'Deep house and Techno all night long, but also good old wine and banter. London, Amsterdam, Barcelona & Berlin in August for work and some partying. Let\'s prance?'
+  },
+  {
+    name: 'Patita Y.',
+    location: 'Sao Paulo',
+    review: 'Sociaholic, always searching for awesome restaurants, terraces, bars, clubs, dj sets, and parties to go to.',
+    image: 'patita.jpg',
+    bio: 'Sociaholic, always searching for awesome restaurants, terraces, bars, clubs, dj sets and parties to go to. Been travelling a lot and currently aspiring to become the Rough Guide to São Paulo myself. Favourite bar in São Paulo: Bebo Sim. No Tinder\'ing here please.'
+  },
+  {
+    name: 'Anna C.',
+    location: 'New York',
+    review: 'Anna is super open and very nice. We went to a quiet party where she was the DJ. She got me in for free and introduced me to her friends. 100% recommendable!',
+    image: 'anna.jpg',
+    bio: 'Annakickarse, the outgoing rebel, is an artistic phenomenon from New York City who has worked her way from nothing to something.  I am skin, bones, blood, flesh & energy. I am and I believe that is all I could truly know about myself for certain. DJ from Webster Hall NYC & resident DJ at QuietEvents.'
+  },
+  {
     name: 'François L.',
     location: 'Lille',
     review: 'Francois is the coolest guy on earth! So much fun and always down to party! He showed me and some friends the best bars in Lille - he is your guy if you want a good night out!',
@@ -58,9 +65,9 @@ var users = [
     bio: 'I live in the Center of Lille. Always ready to meet new persons and to help to discover the city. My buddies and I will be glad to have a walk/to discuss/to have a drink/to party with you !!! ;-)'
   },
   {
-    name: 'Adam U.',
+    name: 'Adam Y.',
     location: 'Amsterdam',
-    review: '',
+    review: 'This guy is just awesome!! Full energy for running and for partying! We met in Amsterdam when he was running through Europe!',
     image: 'adam.jpg',
     bio: 'Australian from Bondi Beach, recently relocated to Amsterdam.'
   },
@@ -70,12 +77,5 @@ var users = [
     review: 'Alex is a super fun & cool guy who knows how to party! Been out partying & getting some drinks at a few bars with him around town a couple of times, I definitely recommend hanging out with Alex, if you want to have an awesome time!! Plus he works at one of the best bars in Amsterdam!',
     image: 'alex.jpg',
     bio: '🔘 easy going guy <br>🔘 social <br>🔘 party animal <br>🔘 crazy <br>🔘 spontaneous <br>Bartender at Regular &Jack 🍺 <br>I love good food & drinks 🍔🌭🍕🍝🌮🌯🍣🍰🍦🍫🍩🍪🍺🍹🍸🍼 & party 🎉🎊 Always up for something. If not I\'m probably working. 💪🏽 In that case you can always come over and have some beers💁🏼 Don\'t be afraid to chat me up🙌🏼 See you soon 🙋🏼'
-  },
-  {
-    name: 'Patita Y.',
-    location: 'Sao Paulo',
-    review: 'Sociaholic, always searching for awesome restaurants, terraces, bars, clubs, dj sets, and parties to go to.',
-    image: 'patita.jpg',
-    bio: 'Sociaholic, always searching for awesome restaurants, terraces, bars, clubs, dj sets and parties to go to. Been travelling a lot and currently aspiring to become the Rough Guide to São Paulo myself. Favourite bar in São Paulo: Bebo Sim. No Tinder\'ing here please.'
   }
 ]
